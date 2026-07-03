a8de9f3cf4976af429be97f7f0464dd93f4e2cda
naman sharma <namansharma031106@gmail.com>
2026-07-03 23:56:47 +0530

    Add files via upload

--- firstclass.java
+++ firstclass.java
@@ -1,31 +1,5 @@
-import java.util.*;
-
 class firstclass {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b= 1*a;
-        int c= 2*a;
-        int d= 3*a;
-        int e= 4*a;
-        int f= 5*a;
-        int g= 6*a;
-        int h= 7*a;
-        int i= 8*a;
-        int j= 9*a;
-        int k= 10*a;
-
-        System.out.println(a + "onza=" +b);
-        System.out.println( a+ "towza=" +c);
-        System.out.println( a+ "threza="+d);
-        System.out.println( a+ "fourza="+e);
-        System.out.println( a+ "fifza="+f);
-        System.out.println( a+ "sixza="+g);
-        System.out.println( a+ "sevenza="+h);
-        System.out.println( a+ "eightza="+i);
-        System.out.println( a+ "nineza="+j);
-        System.out.println( a+ "tenza="+k);
-
-
-        }
+        System.out.println("Hello, World!");
+    }
 }
