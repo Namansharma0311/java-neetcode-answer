41635a78c9210c3f95d7cd8ca5d2894a2e57c1cf
naman sharma <namansharma031106@gmail.com>
2026-07-17 23:54:51 +0530

    Create pakages.java

---
+++ pakages.java
@@ -0,0 +1,15 @@
+import java.util.Random;   // built-in package
+
+public class GFG{
+    
+    public static void main(String[] args) {
+        
+        // using Random class
+        Random rand = new Random();   
+
+        // generates a number between 0–99
+        int number = rand.nextInt(100);  
+
+        System.out.println("Random number: " + number);
+    }
+}
